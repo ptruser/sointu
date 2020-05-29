084e24d7823a17c2799afd9b92207405b963e8da
Veikko Sariola <veikko.sariola@gmail.com>
2020-05-29 22:21:41 +0300

    Drafting.

---
+++ gui/gui.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var app = new Vue({
+      el: '#app',
+      data: {
+        message: 'Hello Vue!'
+      }
+    })
+    
+    var app2 = new Vue({
+      el: '#app-2',
+      data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+      }
+    })
+});
+
--- gui/sointu_client.js
+++ gui/sointu_client.js
@@ -2,37 +2,15 @@ function start(websocketServerLocation) {
     ws = new WebSocket(websocketServerLocation);
 
     ws.onmessage = function (event) {
-        var data = JSON.parse(event.data);
-        Object.keys(data).forEach(k => {
-            if (k == "display") {
-                var show = data[k] == true;
-                document.getElementById("analyzing").style.opacity = !show ? 1 : 0;
-                document.getElementById("results").style.opacity = show ? 1 : 0;
-            } else {
-                var element = document.getElementById(k)
-                element.innerHTML = data[k] + "%";
-                var x = (parseFloat(data[k]) - 50) / 50,
-                    r = Math.round(255 - Math.max(x, 0) * 255),
-                    g = Math.round(255 + Math.min(x, 0) * 255),
-                    b = Math.round(255 - Math.abs(x) * 255),
-                    c = `rgb(${r},${g},${b})`;
-                element.style.color = c;
-            }
-        });
     };
 
     ws.onopen  = function () {
-        document.getElementById("no_connection").style.opacity = 0;
-        document.getElementById("analyzing").style.opacity = 0;
-        document.getElementById("results").style.opacity = 1;
+        const typedArray1 = new Int8Array(8);
+        typedArray1[0] = 1;
+        ws.send(typedArray1);
     };
 
     ws.onclose = function () {
-        document.getElementById("no_connection").style.opacity = 1;
-        document.getElementById("analyzing").style.opacity = 0;
-        document.getElementById("results").style.opacity = 0;
-        // Try to reconnect in 5 seconds
-        setTimeout(function () { start(websocketServerLocation) }, 5000);
     };
 }
 
@@ -41,8 +19,8 @@ function get(name) {
         return decodeURIComponent(name[1]);
 }
 
-window.onload = function () {
+$(document).ready(function(){
     var ip = get('ip') || "127.0.0.1"
     var port = get('port') || "80"
     start("ws://"+ip+":"+port+"/")
-}
+})
